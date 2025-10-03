6376f1a3a675d7aedf342dc245b29ae16631beda
sureock <pulujo2020@gmail.com>
2025-10-03 19:58:18 +0300

    Добавил Laba3, Laba2 и Laba1 изменены

---
+++ Laba3.py
@@ -0,0 +1,28 @@
+a = [x**2 for x in range(1, 11)]
+print(a)
+
+b = [x for x in range(1, 20) if x % 2 == 0]
+print(b)
+
+words = ['python', 'Java', 'c++', 'Rust', 'go']
+c = [x.upper() for x in words if len(x) > 3]
+print(c)
+
+
+class Countdown:
+    def __init__(self, max_value):
+        self.value = 0
+        self.max = max_value
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.value >= self.max:
+            raise StopIteration
+        self.value += 1
+        return self.value
+
+
+for i in Countdown(5):
+    print(i)
