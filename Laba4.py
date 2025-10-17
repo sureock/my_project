e49e59def4d74d7de9bc2de362cb3fe2806a3f2c
sureock <pulujo2020@gmail.com>
2025-10-17 13:58:37 +0300

    Начал работу над следующим заданием, создал класс Car(Transport), использовал super().__init__

--- Laba4.py
+++ Laba4.py
@@ -4,11 +4,23 @@ class Transport:
         self.speed = speed
 
     def __str__(self):
-        return f' Transport: {self.brand}, Speed: {self.speed}\n'
+        return f'Transport: {self.brand}, Speed: {self.speed}'
 
     def move(self):
-        return f'Transport is moving at {self.speed} km/h'
+        return f'\rTransport is moving at {self.speed} km/h'
 
 
-car = Transport('mustang', '70')
+class Car(Transport):
+    def __init__(self, brand, speed, seats):
+        super().__init__(brand, speed)
+        self.seats = seats
+
+    def __str__(self):
+        return super().__str__()+f' Seats: {self.seats}'
+
+    def honk():
+        return 'Beep-Beep!'
+
+
+car = Transport('mustang', '70').Car(6)
 print(car, car.move())
