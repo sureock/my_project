bf8c834f01a0b6cd69d982f40a9e16e18d82f38f
sureock <pulujo2020@gmail.com>
2025-10-17 19:56:32 +0300

    Создал второй декоратор с параметрами

--- wrapper.py
+++ wrapper.py
@@ -1,15 +1,15 @@
 def logger(func):
-    def wrapper(a, b=None, *args, **kwargs):
+    def wrapper(a, b=None):
         print(f'Вызов функции {func.__name__} с'
               f' аргументами {a} и {b}')
         try:
             if b is None:
-                result = func(a, *args, **kwargs)
+                result = func(a)
             else:
-                result = func(a, b, *args, **kwargs)
+                result = func(a, b)
             return f'Функция {func.__name__} вернула результат: {result}'
         except Exception as e:
-            print(f'Произошла ошибка {Exception} - {e}')
+            return f'Произошла ошибка {Exception} - {e}'
     return wrapper
 
 
@@ -25,9 +25,29 @@ def divide(a, b):
 
 @logger
 def greet(name):
-    return f'Привет {name}!'
+    return f'Привет, {name}!'
 
 
 print(add(2, 3))
-print(divide(6, 5))
+print(divide(6, 0))
 print(greet('Alex'))
+
+
+def require_role(allowed_roles):
+    def func_decorator(func):
+        def wrapper(name):
+            if name not in allowed_roles:
+                return f'Доступ запрещен пользователю {name}'
+            else:
+                return func(name)
+        return wrapper
+
+    return func_decorator
+
+
+@require_role(["admin"])
+def delete_database(user):
+    return f'База данных удалена пользователем {user}'
+
+
+print(delete_database('admin'))
