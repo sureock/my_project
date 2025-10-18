a49c1b968640ccc32f40cc4bbc6cbf280a9d9afb
sureock <pulujo2020@gmail.com>
2025-10-18 19:05:17 +0300

    Завершил первичную работу над генератором

--- passgen/generator.py
+++ passgen/generator.py
@@ -1,11 +1,21 @@
 import random
 
 
-def generate():
+def generate(length, s, d, u):
     all_symbols = []
+    result = []
     for i in range(97, 123):
         all_symbols.append(chr(i))
-    return all_symbols
-
-
-print(generate())
+    if s:
+        symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '—', '_', '+', '=', ';', ':', ',', './', '?', '\\', '|', '`', '~', '[', ']', '{', '}']
+        for i in symbols:
+            all_symbols.append(i)
+    if d:
+        for i in range(48, 58):
+            all_symbols.append(i)
+    if u:
+        for i in range(65, 91):
+            all_symbols.append(i)
+    for i in range(length):
+        result.append(random.choice(all_symbols))
+    return result
--- passgen/resourses.py
+++ passgen/resourses.py
@@ -1,8 +1,11 @@
+allr = ['https://www.kaspersky.ru/resource-center/threats/how-to-create-a-strong-password',
+        'https://ps.readthedocs.io/ru/latest/random.html',
+        'https://python-academy.org/ru/guide/modules',
+        'https://symbl.cc/en/unicode-table/#basic-latin',
+        'https://docs.python.org/3/library/hashlib.html',
+        'https://www.azpassword.ru/blog/special-characters-in-the-password']
+
+
 def used_resourses():
-    allr = ['https://www.kaspersky.ru/resource-center/threats/how-to-create-a-strong-password',
-            'https://ps.readthedocs.io/ru/latest/random.html',
-            'https://python-academy.org/ru/guide/modules',
-            'https://symbl.cc/en/unicode-table/#basic-latin',
-            'https://docs.python.org/3/library/hashlib.html']
     for i in allr:
         return i
