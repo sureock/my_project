8e60d8c342a419de6c6b56761ac82dcb43ff749d
sureock <pulujo2020@gmail.com>
2025-10-19 11:10:52 +0300

    Переместил пакет и исполняющий файл в одну папку

---
+++ my_own_package/passgen/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ['commands',
+           'generator',
+           'storage']
+
+print('Пакет passgen загружен')
---
+++ my_own_package/passgen/commands.py
@@ -0,0 +1,41 @@
+import argparse
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="Программа для генерации безопасного пароля"
+        )
+    parser.add_argument(
+        "l",
+        type=int,
+        help="Длина пароля, рекомендуется = 12"
+        )
+    parser.add_argument(
+        "-s", "--special",
+        action="store_true",
+        help="Добавляет специальные символы"
+    )
+    parser.add_argument(
+        "-d", "--digits",
+        action="store_true",
+        help='Добавляет цифры в пароль'
+    )
+    parser.add_argument(
+        "-u", "--upper",
+        action="store_true",
+        help="Добавляет прописные буквы в пароль"
+    )
+    parser.add_argument(
+        "-hash", "--hasher",
+        type=str, choices=["sha256", "md5", "blake2b"],
+        help="Тип хэширования, базовое значение = sha256"
+    )
+    parser.add_argument(
+        "-p", "--path",
+        type=str,
+        action='store',
+        help="Путь сохранения пароля"
+    )
+    args = parser.parse_args()
+    return (args.l, args.special, args.digits,
+            args.upper, args.hasher, args.path)
