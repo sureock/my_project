e89bf6a43463a62ad9c76a79a975b50fe3230f11
sureock <pulujo2020@gmail.com>
2026-02-09 16:30:51 +0300

    Add some pages, changed views (currently doesnt work)

--- semester2/DSTU/catalog/views.py
+++ semester2/DSTU/catalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+# from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def info(request):
+    name = request.GET.get('name', 'Guest')
+    password = request.GET.get('password', 'None')
+    data = {'name': name, 'password': password}
+    return render(request, 'info.html', context=data)
